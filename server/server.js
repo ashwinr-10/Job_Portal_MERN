fb361eabee13afe8b1d526229bedaa898249f26f
J Ashwin Rao <jashwinrao31@gmail.com>
2025-07-15 18:35:40 +0530

    added job controller and route

--- server/server.js
+++ server/server.js
@@ -3,7 +3,9 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
-import userRoutes from "./routes/user.route.js";
+import userRoute from "./routes/user.route.js";
+import companyRoute from "./routes/company.route.js";
+import jobRoute from "./routes/job.route.js";
 
 dotenv.config({});
  
@@ -28,6 +30,7 @@ const PORT = process.env.PORT || 3000;
 //APIs
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
+app.use("/api/v1/job", jobRoute);
 
 app.listen(PORT,()=>{
     connectDB();
