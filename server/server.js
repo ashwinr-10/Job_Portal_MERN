a44a875dd628d8beb5d11f4d952b208335c6d4e0
J Ashwin Rao <jashwinrao31@gmail.com>
2025-07-27 18:12:33 +0530

    updated cors

--- server/server.js
+++ server/server.js
@@ -20,9 +20,10 @@ app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 
 const corsOptions = {
-    origin:'http://localhost:5173',
-    credentials:true
-}
+    origin: ['http://localhost:5173', 'https://proconnectio.vercel.app'],
+    credentials: true
+};
+
 
 app.use(cors(corsOptions));
 
